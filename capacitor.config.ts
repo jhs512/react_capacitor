9e165e7851c65c0b790d0b329e7885ac57845211
jhs512 <jangka512@gmail.com>
2022-03-06 23:57:10 +0900

    안드로이드 프로젝트 추가

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sbs.jhs.exam1',
+  appName: 'exam1',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
